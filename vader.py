87d0deecc4c4542b44b1784ceee0755509d786e9
Nishit Hada <hada.nishit@gmail.com>
2019-04-05 07:55:44 +0530

    Delete vader.py

--- vader.py
+++
@@ -1,8 +0,0 @@
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-sentences = ["VADER is smart, handsome, and funny.", "At least it isn't a horrible book."]
-
-sid = SentimentIntensityAnalyzer()
-for sentence in sentences:
-    print(sentence)
-    ss = sid.polarity_scores(sentence)
-    print(ss['compound'])
