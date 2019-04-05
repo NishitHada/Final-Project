ae9b08dc75e8add1f6baf84e8785f8bdfe131fcd
Nishit Hada <hada.nishit@gmail.com>
2019-04-05 07:55:55 +0530

    Delete text_blob.py

--- text_blob.py
+++
@@ -1,8 +0,0 @@
-from textblob import TextBlob
-
-def Analyze(msg):
-    w=TextBlob(msg)
-    return w.sentiment.polarity
-
-#m = input("Enter text:")
-#print("Sentiment polarity of given text is:"+ str(Analyze(m)))
